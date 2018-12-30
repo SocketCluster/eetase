38fdfc61b7a7bdf3b7758c4255e3722833350cde
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2018-12-30 10:11:17 +0100

    Keep previous EventEmitter functionality

--- test/test.js
+++ test/test.js
@@ -1,7 +1,6 @@
 const assert = require('assert');
 const emtase = require('../index');
 
-const AsyncStreamEmitter = require('async-stream-emitter');
 const EventEmitter = require('events').EventEmitter;
 
 let pendingTimeoutSet = new Set();
@@ -34,103 +33,7 @@ describe('emtase', () => {
     cancelAllPendingWaits();
   });
 
-  it('should replace EventEmitter methods with AsyncStreamEmitter methods', async () => {
-    assert.equal(!emitter._events, true);
-    assert.equal(!emitter._eventsCount, true);
-    assert.equal(!emitter._maxListeners, true);
-
-    let error;
-    try {
-      emitter.getMaxListeners();
-    } catch (err) {
-      error = err;
-    }
-    assert.notEqual(error, null);
-
-    try {
-      emitter.addListener('foo', () => {});
-    } catch (err) {
-      error = err;
-    }
-    assert.notEqual(error, null);
-
-    try {
-      emitter.on('foo', () => {});
-    } catch (err) {
-      error = err;
-    }
-    assert.notEqual(error, null);
-
-    try {
-      emitter.prependListener('foo', () => {});
-    } catch (err) {
-      error = err;
-    }
-    assert.notEqual(error, null);
-
-    try {
-      emitter.once('foo', () => {});
-    } catch (err) {
-      error = err;
-    }
-    assert.notEqual(error, null);
-
-    try {
-      emitter.prependOnceListener('foo', () => {});
-    } catch (err) {
-      error = err;
-    }
-    assert.notEqual(error, null);
-
-    try {
-      emitter.removeListener('foo', () => {});
-    } catch (err) {
-      error = err;
-    }
-    assert.notEqual(error, null);
-
-    try {
-      emitter.off('foo', () => {});
-    } catch (err) {
-      error = err;
-    }
-    assert.notEqual(error, null);
-
-    try {
-      emitter.removeAllListeners('foo');
-    } catch (err) {
-      error = err;
-    }
-    assert.notEqual(error, null);
-
-    try {
-      emitter.listeners('foo');
-    } catch (err) {
-      error = err;
-    }
-    assert.notEqual(error, null);
-
-    try {
-      emitter.rawListeners('foo');
-    } catch (err) {
-      error = err;
-    }
-    assert.notEqual(error, null);
-
-    try {
-      emitter.listenerCount('foo');
-    } catch (err) {
-      error = err;
-    }
-    assert.notEqual(error, null);
-
-    try {
-      emitter.eventNames();
-    } catch (err) {
-      error = err;
-    }
-    assert.notEqual(error, null);
-
+  it('should add AsyncStreamEmitter methods to the EventEmitter instance', async () => {
     assert.equal(!!emitter.emit, true);
     assert.equal(!!emitter.listener, true);
     assert.equal(!!emitter.closeListener, true);
