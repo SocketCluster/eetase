520583921c852c9a02529bd644940356c91a2205
Jonathan Gros-Dubois <grosjona@yahoo.com.au>
2018-12-30 09:58:17 +0100

    Add tests

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # emtase
-EventEmitter to Async Stream Emitter converter.
+EventEmitter to AsyncStreamEmitter converter.
+
+Converts an `EventEmitter` object into a `AsyncStreamEmitter` at runtime.
+
+## Installation
+
+```bash
+npm install emtase
+```
+
+## Usage example
+
+```
+const emtase = require('emtase');
+const http = require('http');
+
+// The Node.js HTTP server is an EventEmitter.
+// Normally works using server.on('request', handler);
+let server = http.createServer();
+
+// ...
+
+emtase(server);
+
+// ...
+
+// The Node.js HTTP server now works as an
+// AsyncStreamEmitter
+(async () => {
+  for await (let req of server.listener('request')) {
+    // ...
+  }
+})();
+```
--- test/test.js
+++ test/test.js
@@ -4,16 +4,37 @@ const emtase = require('../index');
 const AsyncStreamEmitter = require('async-stream-emitter');
 const EventEmitter = require('events').EventEmitter;
 
+let pendingTimeoutSet = new Set();
+
+function wait(duration) {
+  return new Promise((resolve) => {
+    let timeout = setTimeout(() => {
+      pendingTimeoutSet.clear(timeout);
+      resolve();
+    }, duration);
+    pendingTimeoutSet.add(timeout);
+  });
+}
+
+function cancelAllPendingWaits() {
+  for (let timeout of pendingTimeoutSet) {
+    clearTimeout(timeout);
+  }
+}
+
 describe('emtase', () => {
   let emitter;
 
   beforeEach(async () => {
     emitter = new EventEmitter();
+    emtase(emitter);
   });
 
-  it('should convert an EventEmitter instance into an AsyncStreamEmitter', async () => {
-    emtase(emitter);
+  afterEach(async () => {
+    cancelAllPendingWaits();
+  });
 
+  it('should replace EventEmitter methods with AsyncStreamEmitter methods', async () => {
     assert.equal(!emitter._events, true);
     assert.equal(!emitter._eventsCount, true);
     assert.equal(!emitter._maxListeners, true);
@@ -114,5 +135,47 @@ describe('emtase', () => {
     assert.equal(!!emitter.listener, true);
     assert.equal(!!emitter.closeListener, true);
     assert.equal(!!emitter.closeAllListeners, true);
+  });
+
+  it('should support AsyncStreamEmitter functionality', async () => {
+    (async () => {
+      for (let i = 0; i < 10; i++) {
+        await wait(10);
+        emitter.emit('foo', 'hello' + i);
+      }
+      emitter.closeListener('foo');
+    })();
+
+    let receivedData = [];
+    for await (let data of emitter.listener('foo')) {
+      receivedData.push(data);
+    }
+    assert.equal(receivedData.length, 10);
+    assert.equal(receivedData[0], 'hello0');
+    assert.equal(receivedData[9], 'hello9');
+
+    (async () => {
+      for (let i = 0; i < 20; i++) {
+        await wait(10);
+        emitter.emit('bar', 'hi' + i);
+      }
+      emitter.closeAllListeners();
+    })();
+
+    receivedData = [];
+    for await (let data of emitter.listener('bar')) {
+      receivedData.push(data);
+    }
+    assert.equal(receivedData.length, 20);
+    assert.equal(receivedData[0], 'hi0');
+    assert.equal(receivedData[19], 'hi19');
+
+    (async () => {
+      await wait(10);
+      emitter.emit('test', 'abc');
+    })();
+
+    let data = await emitter.listener('test').once();
+    assert.equal(data, 'abc');
   });
 });
